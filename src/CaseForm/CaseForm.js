cc40f3c1df9052f5e6e354faf94fabb2e3a12eba
nikossimaio <nikossimaio@gmail.com>
2021-11-13 01:31:19 +0200

    react-create-app on Heroku

--- src/CaseForm/CaseForm.js
+++ src/CaseForm/CaseForm.js
@@ -1,42 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
+import axios from 'axios';
 
 const LeadForm = () => {
+  const [failed, setFailed] = useState(false)
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = async (data) => {
+    const url='';
+    const response = await axios.post(url, data)
+    console.log(response);
+  };
 
   return (
-    <Form className="border border-secondary p-3" onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Account Id</Form.Label>
-        <Form.Control type="text" placeholder="Account Id" {...register('accountId', { required: true })}/>
-        {errors.accountId && <p>Account Id is required.</p>}
-      </Form.Group>
+    <>
+    <Alert variant='danger'>
+      This is a alert—check it out!
+    </Alert>
+      <Form className="border border-secondary p-3" onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Account Id</Form.Label>
+          <Form.Control type="text" placeholder="Account Id" {...register('accountId', { required: true })}/>
+          {errors.accountId && <p>Account Id is required.</p>}
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Select your case:</Form.Label>
-        <Form.Select aria-label="Default select example" {...register('picklist', { required: true })}>
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </Form.Select>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Select your case:</Form.Label>
+          <Form.Select aria-label="Default select example" {...register('picklist', { required: true })}>
+            <option>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Place any text</Form.Label>
-        <Form.Control as="textarea" rows={3}  {...register('text', { required: true })}/>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Place any text</Form.Label>
+          <Form.Control as="textarea" rows={3}  {...register('text', { required: true })}/>
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   )
 }
 
